57abcd7590dec26c3a740debef07efbff3051e7c
E Ocean <sibte.hunain@affinitysuite.net>
2024-07-20 15:02:50 +0500

    added filters to the report

--- po_report/report/report.py
+++ po_report/report/report.py
@@ -7,12 +7,52 @@ class CustomReport(models.AbstractModel):
     _name = "report.po_report.po_reports"
     _description = "Purchase Order Report"
 
+
     def _get_report_values(self, docids, data=None):
+        
+    #     product_ids = fields.Many2many('product.template', string='Product', required=True)
+    # warehouse_id = fields.Many2one('stock.warehouse', string = "Ware House")
+    # vendor = fields.Char(string = "Vendor")
+    # po_no = fields.Char(string = "po_no")
+    # grn = fields.Char(string = "grn")
+    # invoice_no = fields.Char(string = "invoice_no")
+    # pr_no = fields.Char(string = "pr_no")
+
+        
+        other_details = {}
+        date_from = data['date_from']
+        date_to = data['date_to']
+        product_ids = data['product_ids']
+        warehouse_id = data['warehouse_id']
+        vendor = data['vendor']
+        po_no = data['po_no']
+        grn = data['grn']
+        invoice_no = data['invoice_no']
+        pr_no = data['pr_no']
+
+
+        other_details.update({
+                'from_date': date_from,
+                'to_date': date_to,
+                'product_ids': product_ids,
+                'warehouse_id': warehouse_id,
+                'vendor': vendor,
+                'po_no': po_no,
+                'grn': grn,
+                'invoice_no': invoice_no,
+                'pr_no' : pr_no
+            })
+        
+        if product_ids != []:
+            product_ids_str = ','.join(map(str,product_ids))
+
+        cr = self._cr
         query = ("""
                 SELECT 
                     po.date_order AS date,
+                    po.partner_ref AS vendor,
                     pol.name AS item,
-                    po.origin as reqno,
+                    po.origin as prno,
                     po.name AS pono,
                     sp.name AS grn,
                     am.name AS invoiceno,
@@ -25,23 +65,60 @@ class CustomReport(models.AbstractModel):
                     sw.name as location
                 FROM purchase_order_line pol 
                 INNER JOIN purchase_order po ON po.id = pol.order_id
+                inner JOIN product_product pp ON pp.id = pol.product_id
+                inner JOIN product_template pt ON pt.id = pp.product_tmpl_id
                 Left JOIN stock_picking sp ON sp.origin = po.name   
                 Left JOIN account_move am ON am.invoice_origin = po.name
                 inner join stock_picking_type spt on spt.id = po.picking_type_id
                 inner join stock_warehouse sw on sw.id = spt.warehouse_id
                 inner join uom_uom mm on mm.id = pol.product_uom
+                
+                WHERE
+                    WHERE po.date_order BETWEEN '%s' AND '%s'
+                    AND pt.id in (%s)
+                    AND spt.warehouse_id = %s
+                    AND po.partner_ref = %s
+                    AND spt.warehouse_id = %s
+                    AND po.name = %s
+                    AND sp.name = %s
+                    AND am.name = %s
+                    AND pr.name = %s
+                    
+                    
+                 
                 order by po.name
-    """
-                        ) 
-     
-        cr = self._cr
-        cr.execute(query)
-        result = cr.dictfetchall()
-
-        # raise UserError(str(result))
+                
+                """
         
+        % (date_from, date_to,product_ids_str,warehouse_id, vendor, po_no, grn, invoice_no, pr_no) )
+        #  % (date_from,date_to)
+# ,category_ids,stock_location_id
+        cr.execute(query)
+        data = cr.dictfetchall()
+
         return {
-            'data': result,
+            'doc_ids': docids,
+            'data': data,
+            'other': other_details,
         }
+
+         
+#         Item Wise
+# Vendor Wise
+# Warehouse Wise
+# PO No
+# GRN
+# Requisation No
+# Invoice No
+
+     
+        # cr.execute(query)
+        # result = cr.dictfetchall()
+
+        # # raise UserError(str(result))
+        
+        # return {
+        #     'data': result,
+        # }
 
         
--- po_report/wizard/wizard.py
+++ po_report/wizard/wizard.py
@@ -5,6 +5,35 @@ class POReportWizard(models.TransientModel):
     _name = 'po.report'
     _description = 'Purchase Order Report'
     
-  
+    date_from = fields.Date(string='From Date', required=True)
+    date_to = fields.Date(string='To Date', required=True)
+    product_ids = fields.Many2many('product.template', string='Product', required=True)
+    warehouse_id = fields.Many2one('stock.warehouse', string = "Ware House", required=True)
+    vendor = fields.Char(string = "Vendor", required=True)
+    po_no = fields.Char(string = "po_no", required=True)
+    grn = fields.Char(string = "grn", required=True)
+    invoice_no = fields.Char(string = "invoice_no", required=True)
+    pr_no = fields.Char(string = "pr_no", required=True)
+
+    
+
     def print_report(self):
-        return self.env.ref('po_report.po_report_pdf').with_context(landscape=True).report_action(self)
+
+        product_ids = []
+        if self.product_ids:
+            for id in self.product_ids:
+                product_ids.append(id.id)
+        
+        data = {
+            'date_from': self.date_from,
+            'date_to': self.date_to,
+            'product_ids': product_ids,
+            'warehosue_id': self.warehouse_id.id,
+            'vendor': self.vendor,
+            'po_no': self.po_no,
+            'grn': self.grn,
+            'invoice_no': self.invoice_no,
+            'pr_no' : self.pr_no
+            }
+
+        return self.env.ref('po_report.po_report_pdf').with_context(landscape=True).report_action(self, data=data)
