d9140cc50006572df1e6dfe6d5cf1c3a8d5d0e69
E Ocean <sibte.hunain@affinitysuite.net>
2024-08-16 10:51:18 +0500

    added some fields on customer from

--- affinity_nasa_ext/models/affinity_ext.py
+++ affinity_nasa_ext/models/affinity_ext.py
@@ -4,3 +4,12 @@ class ProductCategoryInherited(models.Model):
     _inherit = 'product.category'
 
     company_id = fields.Many2one('res.company', string = 'Companies', default=lambda self: self.env.company.name)
+
+class ResPartnerInherited(models.Model):
+    _inherit = "res.partner"
+
+    legal_status = fields.Selection([('individual', 'Individual'), ('partnership', 'Partnership'), ('aop', 'AOP'), ('company', 'Company')], required = True)
+    company_reg = fields.Char("Company Registration", required = True)
+    artical = fields.Char("Article of Association and Memorandom", required = True)
+    strn_no = fields.Char("STRN", required = True)
+    cnic_no = fields.Char("CNIC Number", required = True)
