07cf189b01cca7ea45cb75950fe291754b4c2881
E Ocean <sibte.hunain@affinitysuite.net>
2024-08-16 11:12:39 +0500

    //

--- affinity_nasa_ext/models/affinity_ext.py
+++ affinity_nasa_ext/models/affinity_ext.py
@@ -1,9 +1,9 @@
 from odoo import fields, models, api
 
-class ProductCategoryInherited(models.Model):
-    _inherit = 'product.category'
+# class ProductCategoryInherited(models.Model):
+#     _inherit = 'product.category'
 
-    company_id = fields.Many2one('res.company', string = 'Companies')
+#     company_id = fields.Many2one('res.company', string = 'Companies')
 
 class ResPartnerInherited(models.Model):
     _inherit = "res.partner"
