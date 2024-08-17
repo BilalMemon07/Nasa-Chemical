d8364705689ed494ae808ce315f1e0413ef7be11
E Ocean <sibte.hunain@affinitysuite.net>
2024-08-17 11:38:18 +0500

    added some fields in PR line

--- affinity_nasa_ext/models/affinity_ext.py
+++ affinity_nasa_ext/models/affinity_ext.py
@@ -5,3 +5,9 @@ class ProductCategoryInherited(models.Model):
 
     company_id = fields.Many2one('res.company', string = 'Companies', default=lambda self: self.env.company)
 
+class PurchaseRerquestLineInherited(models.Model):
+    _inherit = 'purchase.request.line'
+
+    minimum_stock_level = fields.Char('Minimum Stock Level')
+    forcasting_stock = fields.Char('Forcasting Stock')
+    date_due = fields.Date('Date Due')
