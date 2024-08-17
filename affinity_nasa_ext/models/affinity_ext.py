8e6c60f165d2f8146752d05187f67371ac947b41
E Ocean <sibte.hunain@affinitysuite.net>
2024-08-17 15:13:59 +0500

    Update affinity_ext.py

--- affinity_nasa_ext/models/affinity_ext.py
+++ affinity_nasa_ext/models/affinity_ext.py
@@ -8,14 +8,14 @@ class ProductCategoryInherited(models.Model):
 class PurchaseRerquestLineInherited(models.Model):
     _inherit = 'purchase.request.line'
 
-    minimum_stock_level = fields.Char('Minimum Stock Level', compute="_compute_stock_level")
+    minimum_stock_level = fields.Char('Minimum Stock Level')
     forcasting_stock = fields.Char('Forcasting Stock')
     date_due = fields.Datetime('Date Due')
 
     
 
-    @api.depends('product_id')
-    def _compute_stock_level(self):
-        for rec in self:
-            order = self.env['stock.warehouse.orderpoint'].search([('product_id', '=', rec.product_id)])
-            rec['minimum_stock_level'] = order.product_min_qty
+    # @api.depends('product_id')
+    # def _compute_stock_level(self):
+    #     for rec in self:
+    #         order = self.env['stock.warehouse.orderpoint'].search([('product_id', '=', rec.product_id)])
+    #         rec['minimum_stock_level'] = order.product_min_qty
