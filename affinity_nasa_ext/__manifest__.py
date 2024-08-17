5492c4355c10aa0620c4a9eaa237f4370c1358ca
E Ocean <sibte.hunain@affinitysuite.net>
2024-08-17 11:41:24 +0500

    Update __manifest__.py

--- affinity_nasa_ext/__manifest__.py
+++ affinity_nasa_ext/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Affinity Nasa Extension',
     'version' : '1.0.0',
     'summary' : 'For Customization Purpose',
-    'depends' : ['base', 'product'],
+    'depends' : ['base', 'product', 'purchase'],
     'data' : ['views/affinity_ext.xml',],
     'installable' : True,
     'application' : True,
