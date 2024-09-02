6ce657e5a2178d9d491cb51915da9af10ca63729
E Ocean <sibte.hunain@affinitysuite.net>
2024-09-02 13:03:58 +0500

    Update affinity_ext.py

--- affinity_nasa_ext/models/affinity_ext.py
+++ affinity_nasa_ext/models/affinity_ext.py
@@ -12,8 +12,7 @@ class PurchaseRerquestLineInherited(models.Model):
     minimum_stock_level = fields.Char('Minimum Stock Level')
     forcasting_stock = fields.Char('Forcasting Stock')
     date_due = fields.Datetime('Date Due', required = True)
-
-    
+  
 
     # @api.depends('product_id')
     # def _compute_stock_level(self):
